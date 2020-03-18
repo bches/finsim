4b25928093482d76433de9df70dba6c8f15c0243
Brian Chesney <bpchesney@gmail.com>
2020-03-17 20:36:38 -0500

    Update account.py
    added adjusting entries object

--- accounting/account.py
+++ accounting/account.py
@@ -62,6 +62,27 @@ def accounting_print(x):
   return ''
   
   
+class adjusting_entry:
+  def __init__(self, debit_account, credit_account):
+    self.debit_account = debit_account
+    self.credit_account = credit_account
+    self.amount = 0
+    self.description = ''
+
+  def __call__(self, amount, description):
+    self.amount = amount
+    self.description = description
+    self.debit_account.debit(amount)
+    self.credit_account.credit(amount)
+
+  def __repr__(self):
+    s = '{:20}{:>10}{:>10}\n{:20}{:>10}\n     {:20}{:>15}\n{}'.format(self.__class__.__name__,'Dr','Cr',
+                                  self.debit_account.label, self.amount,
+                                  self.credit_account.label, self.amount,
+                                  self.description)
+    return s
+
+  
 if __name__ == '__main__':
   cash = asset_account('Cash', 101)
   cash.debit(2700)
