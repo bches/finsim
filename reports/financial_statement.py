4394c7894938fa7800b95f4887fa2a39f911d8b5
Brian Chesney <bpchesney@gmail.com>
2020-03-20 20:28:22 -0500

    Create financial_statement.py
    Virtual base class for the balance_sheet, statement of shareholders equity and income statement objects

---
+++ reports/financial_statement.py
@@ -0,0 +1,14 @@
+class financial_statement:
+    '''Virtual base class for the other financial statements
+    def __init__(self, co, periodicity, timestamp):
+        self.co = co
+        self.statement = 'Financial Statement'
+        self.ending = 'For the %s Ending %s' % (periodicity, timestamp.ctime())
+
+    def __repr__(self):
+        company = self.co.name
+        statement = self.statement
+        ending = self.ending
+        return f'{company}\n\
+{statement}\n\
+{ending}\n\n'
