c16e8d55f32d66bd21b6b7c5466f85bdd8a06d80
bches <bpchesney@gmail.com>
2020-03-20 23:44:24 -0500

    added accounts to contract and promissory note

--- contracts/contract.py
+++ contracts/contract.py
@@ -2,10 +2,16 @@ class contract:
     '''A contract is a virtual base class for financial agreements
     between companies or individuals'''
     def __init__(self, payee, payer, amount):
+        assert 'Cash' in payee, 'Payee needs a Cash account'
+        assert 'Cash' in payer, 'Payer needs a Cash account'
+        assert 'Accounts Receivable' in payee, 'Payee needs an Accounts Receivable account'
+        assert 'Accounts Payable' in payer, 'Payer needs an Accounts Payable account'
         self.payee = payee
         self.payer = payer
         self.balance = amount
-
+        self.payer['Accounts Payable'].increase(amount)
+        self.payee['Accounts Receivable'].increase(amount)
+        
     def __repr__(self):
         cls = self.__class__.__name__
         payee = self.payee
@@ -17,12 +23,29 @@ class contract:
     def pay(self, amount):
         assert amount > 0, "Must pay amount > 0."
         assert amount <= self.balance, "Cannot pay more than the balance"
+        self.payer['Cash'].decrease(amount)
+        self.payer['Accounts Payable'].decrease(amount)
+        self.payee['Cash'].increase(amount)
+        self.payee['Accounts Receivable'].decrease(amount)
         self.balance -= amount
 
 
 if __name__ == '__main__':
-    dut = contract(payee='me', payer='you', amount=12)
+    from ..accounting.portfolio import portfolio
+
+    me = portfolio(name='My Company')
+    print('me =', me)
+    
+    you = portfolio(name='Your Comoany')
+    print('you =', you)
+
+    print()
+
+    dut = contract(payee=me, payer=you, amount=12)
     print(dut)
     print()
+    
     dut.pay(5)
     print(dut)
+    print()
+    
--- contracts/promissory_note.py
+++ contracts/promissory_note.py
@@ -6,29 +6,64 @@ class promissory_note(bad_debt):
         bad_debt.__init__(self, payee, payer, amount)
         assert 'Interest Receivable' in self.payee, 'Payee needs an Interest Receivable account'
         assert 'Interest Revenue' in self.payee, 'Payee needs Interest Revenue account'
+        assert 'Interest Payable' in self.payer, 'Payer needs an Interest Payable account'
+        assert 'Interest Expense' in self.payer, 'Payer needs Interest Expense account'
+        assert term > 1, 'Term must be greater than 1'
         self.interest_rate = interest_rate
         self.term = term
-
+        r = interest_rate
+        n = term
+        self.payment = amount * r / (1 - 1 / ((1+r) ** n))
+        self.total_interest = term * self.payment - amount
+        self.payee['Interest Receivable'].increase(self.total_interest)
+        self.payer['Interest Payable'].increase(self.total_interest)
+        
     def __repr__(self):
         s = bad_debt.__repr__(self)
         interest_rate = self.interest_rate
+        payment = self.payment
         term = self.term
-        s += '\n\tinterest_rate = {interest_rate}'
-        s += '\n\tterm = {term}'
+        s += '\n\tinterest_rate = %s' % interest_rate
+        s += '\n\tpayment = %s' % payment
+        s += '\n\tterm = %s' % term
         return s
 
     def __iter__(self):
         for i in range(self.term):
-            interest = self.interest_rate * self.balance
-            payee['Interest Receivable'].debit(interest)
-            payee['Interest Revenue'].credit(interest)
-            self.balance += interest
+            self.pay(min(self.payment, self.balance))
+            interest = self.total_interest / self.term
+            self.payee['Interest Receivable'].decrease(interest)
+            self.payee['Interest Revenue'].increase(interest)
+            self.payer['Interest Payable'].decrease(interest)
+            self.payer['Interest Expense'].increase(interest)
             yield
 
 
 if __name__ == '__main__':
-    from ..examples.big_dog_carworks import BigDog
+    from ..accounting.portfolio import portfolio
+
+    me = portfolio(name='My Company')
+    me.add_revenue_category(name='Interest Revenue', code=560)
+    me.add_asset_category(name='Interest Receivable', code=160)
+    print('me =', me)
     
-    dut = promissory_note(payee=BigDog, payer=None,
-                          amount=5000, interest_rate=0.001,
-                          term=2)
+    you = portfolio(name='Your Comoany')
+    you.add_expense_category(name='Interest Expense', code=660)
+    you.add_liability_category(name='Interest Payable', code=220)
+    
+    print('you =', you)
+
+    print()
+
+    dut = promissory_note(payee=me, payer=you,
+                          amount=5000, interest_rate=0.01,
+                          term=12)
+    print(dut)
+    print()
+
+    for payment in dut:
+        print('Paying...')
+    
+    print(dut)
+    print()
+    
